55111fd36512b1237219aa9fd4e98ff7faf90e53
Charlatan2k <octavianmihai.calin@gmail.com>
2024-09-12 23:06:48 +0200

    Finished rock paper scissors

---
+++ main.js
@@ -0,0 +1,49 @@
+function getComputerChoice() {
+  let computerChoice = Math.round(Math.random() * 2 + 1);
+  if (computerChoice == 1) {
+    computerChoice = 'Rock';
+  } else if (computerChoice == 2) {
+    computerChoice = 'Paper';
+  } else {
+    computerChoice = 'Scissors';
+  }
+  return computerChoice;
+}
+
+function getHumanChoice() {
+  let humanChoice = prompt('Choose Rock Paper or Scissors');
+  return humanChoice;
+}
+
+function playGame() {
+  let computerScore = 0;
+  let humanScore = 0;
+  function playRound() {
+    const humanChoice = getHumanChoice();
+    const computerChoice = getComputerChoice();
+    if (humanChoice === computerChoice) {
+      alert('It is a draw');
+    } else if (
+      (humanChoice == 'Scissors' && computerChoice == 'Paper') ||
+      (humanChoice == 'Paper' && computerChoice == 'Rock') ||
+      (humanChoice == 'Rock' && computerChoice == 'Scissors')
+    ) {
+      alert('You win!');
+      humanScore += 1;
+    } else if (
+      (computerChoice == 'Rock' && humanChoice == 'Scissors') ||
+      (computerChoice == 'Paper' && humanChoice == 'Rock') ||
+      (computerChoice == 'Scissors' && humanChoice == 'Paper')
+    ) {
+      alert('You lost.. PC wins');
+      computerScore += 1;
+    }
+    alert('Your score is:' + humanScore + 'PC score is :' + computerScore);
+  }
+  playRound();
+  playRound();
+  playRound();
+  playRound();
+}
+
+playGame();
